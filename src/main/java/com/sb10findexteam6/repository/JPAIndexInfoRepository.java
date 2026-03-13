90f40de8049bafdbe17e3dc57e75d103f0c9b1e6
jongin-git <jongin33@gmail.com>
2026-03-13 17:41:58 +0900

    fix: 임시 파일 삭제

--- src/main/java/com/sb10findexteam6/repository/JPAIndexInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sb10findexteam6.repository;
-
-import com.sb10findexteam6.entity.IndexInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JPAIndexInfoRepository extends JpaRepository<IndexInfo, Long> {
-
-}
