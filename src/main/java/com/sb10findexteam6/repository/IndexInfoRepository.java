9a90ec96d8d6c32a7857cbed1c186d87d219a9a3
JunSuHwang <wnstn9904@naver.com>
2026-03-13 17:52:16 +0900

    refactor: IndexInfoRepository로 이름 수정

--- src/main/java/com/sb10findexteam6/repository/IndexInfoRepository.java
+++ src/main/java/com/sb10findexteam6/repository/IndexInfoRepository.java
@@ -3,6 +3,6 @@ package com.sb10findexteam6.repository;
 import com.sb10findexteam6.entity.IndexInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JPAIndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+public interface IndexInfoRepository extends JpaRepository<IndexInfo, Long> {
 
 }
