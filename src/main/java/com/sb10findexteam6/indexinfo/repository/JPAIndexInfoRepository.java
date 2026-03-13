f0d49aff6b8713913b7450b84193babc64d3b109
안승리 <hauall@naver.com>
2026-03-13 15:17:59 +0900

    IndexInfo 엔티티 및 레포지토리 생성

---
+++ src/main/java/com/sb10findexteam6/indexinfo/repository/JPAIndexInfoRepository.java
@@ -0,0 +1,8 @@
+package com.sb10findexteam6.indexinfo.repository;
+
+import com.sb10findexteam6.indexinfo.entity.IndexInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JPAIndexInfoRepository extends JpaRepository<IndexInfo, Long> {
+
+}
