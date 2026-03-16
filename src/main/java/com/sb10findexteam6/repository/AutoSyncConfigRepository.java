12bb65c5d86fc3cf55a873d9a55e8552461bb44d
안승리 <hauall@naver.com>
2026-03-16 18:37:26 +0900

    지수정보 자동 등록

--- src/main/java/com/sb10findexteam6/repository/AutoSyncConfigRepository.java
+++ src/main/java/com/sb10findexteam6/repository/AutoSyncConfigRepository.java
@@ -1,7 +1,9 @@
 package com.sb10findexteam6.repository;
 
 import com.sb10findexteam6.entity.AutoSyncConfig;
+import com.sb10findexteam6.entity.IndexInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AutoSyncConfigRepository extends JpaRepository<AutoSyncConfig, Long> {
+  void deleteByIndexInfo(IndexInfo indexInfo);
 }
