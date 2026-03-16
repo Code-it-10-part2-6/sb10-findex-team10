6c45e27db79416d52e84105278f89c08d89f2edc
jiho0420 <jiho0420@gachon.ac.kr>
2026-03-16 17:40:46 +0900

    Merge develop into feat/scheduler

--- src/main/java/com/sb10findexteam6/Sb10FindexTeam6Application.java
+++ src/main/java/com/sb10findexteam6/Sb10FindexTeam6Application.java
@@ -3,9 +3,11 @@ package com.sb10findexteam6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 public class Sb10FindexTeam6Application {
 
     public static void main(String[] args) {
