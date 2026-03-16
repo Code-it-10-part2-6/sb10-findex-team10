d1da7d75ea821a3b3fc6b0eead7ce2ea509e8532
jiho0420 <jiho0420@gachon.ac.kr>
2026-03-16 20:34:12 +0900

    Mock객체를 활용한 test 진행

--- src/main/java/com/sb10findexteam6/Sb10FindexTeam6Application.java
+++ src/main/java/com/sb10findexteam6/Sb10FindexTeam6Application.java
@@ -3,11 +3,9 @@ package com.sb10findexteam6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableJpaAuditing
 public class Sb10FindexTeam6Application {
     public static void main(String[] args) {
         SpringApplication.run(Sb10FindexTeam6Application.class, args);
