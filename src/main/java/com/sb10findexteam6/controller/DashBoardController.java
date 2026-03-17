63b13a4a3004fe756d2a8a559f7f0a2764a62063
jongin-git <jongin33@gmail.com>
2026-03-17 17:22:27 +0900

    refactor: api 명세 맞춰서 tag 완료

--- src/main/java/com/sb10findexteam6/controller/DashBoardController.java
+++ src/main/java/com/sb10findexteam6/controller/DashBoardController.java
@@ -2,6 +2,8 @@ package com.sb10findexteam6.controller;
 
 import com.sb10findexteam6.dto.dashboard.*;
 import com.sb10findexteam6.service.DashBoardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "지수 데이터 API", description = "지수 데이터 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping
@@ -22,12 +25,14 @@ public class DashBoardController {
     /**
      * 주요 지수 현황 요약 (즐겨찾기 지수 최신 종가/대비/등락률 리스트)
      */
+    @Operation(summary = "관심 지수 성과 조회")
     @GetMapping("/api/index-data/performance/favorite")
     public ResponseEntity<List<IndexPerformanceDto>> getFavoriteIndexesPerformance() {
         List<IndexPerformanceDto> result = dashBoardService.getFavoriteIndexesPerformance();
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "지수 차트 조회")
     @GetMapping("/api/index-data/{id}/chart")
     public ResponseEntity<IndexChartDto> getIndexChart(
         @PathVariable Long id,
@@ -37,6 +42,7 @@ public class DashBoardController {
     }
 
      // 지수 성과 랭킹 조회
+     @Operation(summary = "지수 성과 랭킹 조회")
      @GetMapping("/api/index-data/performance/rank")
      public ResponseEntity<List<RankedIndexPerformanceDto>> getPerformanceRank(
              @RequestParam(required = false) Long indexInfoId,
