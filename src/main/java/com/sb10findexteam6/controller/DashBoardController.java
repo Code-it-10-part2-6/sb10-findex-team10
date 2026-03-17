6f219be514b93db62ba27ed3553df06f68ec9715
JunSuHwang <wnstn9904@naver.com>
2026-03-17 11:24:52 +0900

    fix: api 경로 오류 수정

--- src/main/java/com/sb10findexteam6/controller/DashBoardController.java
+++ src/main/java/com/sb10findexteam6/controller/DashBoardController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/dashboard")
+@RequestMapping
 public class DashBoardController {
 
     private final DashBoardService dashBoardService;
