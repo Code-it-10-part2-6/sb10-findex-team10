cd123274c4a48a1817cc439e3a46a709e0317c4d
JONGIN CHOI <jongin33@gmail.com>
2026-03-17 18:00:41 +0900

    API Tag 설정 완료 (#79)
    * refactor: api 오류 수정
    
    * refactor: api tag 설정 완료

--- src/main/java/com/sb10findexteam6/controller/SyncJobController.java
+++ src/main/java/com/sb10findexteam6/controller/SyncJobController.java
@@ -8,6 +8,9 @@ import com.sb10findexteam6.dto.syncJob.SyncJobDto;
 import com.sb10findexteam6.dto.syncJob.SyncJobSearchCondition;
 import com.sb10findexteam6.dto.syncJob.SyncJobSummaryDto;
 import com.sb10findexteam6.service.SyncJobService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -17,13 +20,14 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "연동 작업 API", description = "연동 작업 관리 API")
 @RestController
 @RequestMapping("/api/sync-jobs")
 @RequiredArgsConstructor
 public class SyncJobController {
 
     private final SyncJobService syncJobService;
-
+    @Operation(summary = "연동 작업 목록 조회")
     @GetMapping
     public ResponseEntity<CursorPageResponseSyncJobDto> getSyncJobList(
             @RequestParam(required = false) JobType jobType,
@@ -55,11 +59,13 @@ public class SyncJobController {
         return ResponseEntity.ok(syncJobService.getAll(condition));
     }
 
+    @Hidden
     @GetMapping("/summary")
     public ResponseEntity<SyncJobSummaryDto> getSyncJobSummary() {
         return ResponseEntity.ok(syncJobService.getSummary());
     }
 
+    @Operation(summary = "지수 데이터 연동")
     @PostMapping("/index-data")
     public ResponseEntity<List<SyncJobDto>> syncIndexData(
             @RequestBody IndexDataSyncRequest request,
@@ -69,7 +75,7 @@ public class SyncJobController {
         return ResponseEntity.accepted()
                 .body(syncJobService.syncIndexData(request, worker));
     }
-
+    @Operation(summary = "지수 정보 연동")
     @PostMapping("/index-infos")
     public ResponseEntity<List<SyncJobDto>> syncIndexInfos(HttpServletRequest httpServletRequest) {
         String worker = extractWorker(httpServletRequest);
